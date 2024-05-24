2a2e2b1a95ec198b49614873402960825cb03d78
MiaaCao <129354849+MiaaCao@users.noreply.github.com>
2024-05-24 14:32:39 +1200

    Delete Q2 directory

--- Q2/Q2.R
+++
@@ -1,5 +0,0 @@
-# Install package dplyr that used for the manipulation and display of data within a data frame
-install.packages("dplyr") 
-library(dplyr)
-
-dataset.df <- read.csv("./Excel File/Infectious Disease 2001-2014.csv")
